7996807b2b88c8fd37c749198e21e40558ee5624
Argelia  Cardenas <argecardenas@Argelias-Air.attlocal.net>
2020-07-09 15:50:38 -0700

    weather flask app

---
+++ app1.py
@@ -0,0 +1,12 @@
+# Import flask dependency
+#from flask import Flask
+
+# Create a new flask app instance
+#app = Flask(__name__)
+
+
+# Create Flask Routes
+#@app.route('/')
+#def hello_world():
+    #return 'Hello world'
+    
